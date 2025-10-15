f41c5f8d03dac39e50fffeda6b7b9aeb327b2f13
Piyawat Intarawichai <piyawat@orcsoft.co.th>
2025-10-15 15:26:51 +0700

    initial project

---
+++ src/main/java/com/example/cicd/GithubCicdActionsApplication.java
@@ -0,0 +1,23 @@
+package com.example.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to my application!";
+	}
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
