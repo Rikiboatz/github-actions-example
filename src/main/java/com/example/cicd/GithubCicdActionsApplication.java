b92a6971cf70459bc2108ee6dd75f1714246dbcb
Piyawat Intarawichai <piyawat@orcsoft.co.th>
2025-10-15 17:01:04 +0700

    update test

--- src/main/java/com/example/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/example/cicd/GithubCicdActionsApplication.java
@@ -1,8 +1,13 @@
 package com.example.cicd;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -10,14 +15,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 public class GithubCicdActionsApplication {
 
+	public static Logger logger = LoggerFactory.getLogger(GithubCicdActionsApplication.class);
+
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "Welcome to my application!";
+	}
+
+	@PostConstruct
+	public void init() {
+		logger.info("Application stated...");
 	}
 	
 	// update
 
 	public static void main(String[] args) {
+		logger.info("Application executed...");
 		SpringApplication.run(GithubCicdActionsApplication.class, args);
 	}
 
