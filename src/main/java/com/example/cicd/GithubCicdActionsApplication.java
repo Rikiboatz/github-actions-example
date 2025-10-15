93910de66f4a45aaf408ecc7c3f0bdaeb970af12
Piyawat Intarawichai <piyawat@orcsoft.co.th>
2025-10-15 15:31:55 +0700

    update

--- src/main/java/com/example/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/example/cicd/GithubCicdActionsApplication.java
@@ -15,6 +15,7 @@ public class GithubCicdActionsApplication {
 		return "Welcome to my application!";
 	}
 	
+	// update
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubCicdActionsApplication.class, args);
