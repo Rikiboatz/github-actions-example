b45477f55c652196d3d68790f1d244955c839634
Piyawat Intarawichai <piyawat@orcsoft.co.th>
2025-10-17 13:21:45 +0700

    update application-prod.propreties

--- src/test/java/com/example/cicd/controller/UserIntegrationTest.java
+++ src/test/java/com/example/cicd/controller/UserIntegrationTest.java
@@ -1,7 +1,6 @@
 package com.example.cicd.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
